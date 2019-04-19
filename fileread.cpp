f400004506a03c3bd3e540d3d2192e5c9eb4eb56
Jackson Trust <49181480+JacksonTrust@users.noreply.github.com>
2019-04-18 19:21:36 -0600

    Update fileread.cpp

--- fileread.cpp
+++ fileread.cpp
@@ -3,43 +3,41 @@
 #include <cstdio>
 #include <cstddef> // for std::size_t -> is a typedef on an unsinged int
 #include <climits>
-using namespace std ;
+using namespace std;
 //type handling for input and output use BOOST FILESYSTEM LIBRARY
 
 long long * inFiProbs(string inFilename, string oFile){
-  ifstream inFile;//creatae ifstream;
+	ifstream inFile;//creatae ifstream;
 	inFile.open( inFilename, ios::in|ios::binary);//open file as binary input
-  if(!(inFile.is_open()) || !(inFile.good())){//check if open and good
-    cout<<"File Not Opened."<<endl;
-    inFile.close();
-    return nullptr;
-  }
+	if(!(inFile.is_open()) || !(inFile.good())) {//check if open and good
+		cout<<"File Not Opened."<<endl;
+		inFile.close();
+		return nullptr;
+  	}
+	
+	long long * probability = new long long [UCHAR_MAX + 1];//creates probability array
+	for( int i = 0; i <= UCHAR_MAX ; i++){//set all counts to zero //using unsigned char ++ overloads for() ++ iteratore????
+		probability[i] = 0;
+	}
 
-  long long * probability = new long long [UCHAR_MAX + 1];//creates probability array
+  	unsigned char ILikeTurtles = 0; //temp storage of single byte
+  	std::streambuf * inF = inFile.rdbuf();//creates accesible stream buffer
+	ofstream outf ( oFile, std::ofstream::binary| std::ofstream::trunc);//creates output file of same type               //USED FOR TESTING: REMOVE
+	
+	while( inF->sgetc() != EOF){ //checks for valid input
+    		ILikeTurtles = inF->sbumpc();//gets uchar from file moves to next position in streambuf
+  		//  cout<<":"<<ILikeTurtles                                                                                  //USED FOR TESTING: REMOVE
+    		probability[ILikeTurtles]++;
+    		outf<<ILikeTurtles;//outputs uchar to file                                                                   //USED FOR TESTING: REMOVE
+  	}
 
+  	for( ILikeTurtles = 0; ILikeTurtles < UCHAR_MAX; ILikeTurtles++ )//prints counts of each uchar from inFile            //USED FOR TESTING: REMOVE
+    		cout<<ILikeTurtles<<probability[ILikeTurtles]<<endl;                                                          //USED FOR TESTING: REMOVE
 
-  for( int i = 0; i <= UCHAR_MAX ; i++){//set all counts to zero //using unsigned char ++ overloads for() ++ iteratore????
-   probability[i] = 0;}
-
-  unsigned char ILikeTurtles = 0; //temp storage of single byte
-  std::streambuf * inF = inFile.rdbuf();//creates accesible stream buffer
-
-  ofstream outf ( oFile, std::ofstream::binary| std::ofstream::trunc);//creates output file of same type               //USED FOR TESTING: REMOVE
-
-  while( inF->sgetc() != EOF){ //checks for valid input
-    ILikeTurtles = inF->sbumpc();//gets uchar from file moves to next position in streambuf
-  //  cout<<":"<<ILikeTurtles                                                                                                //USED FOR TESTING: REMOVE
-    probability[ILikeTurtles]++;
-    outf<<ILikeTurtles;//outputs uchar to file                                                                               //USED FOR TESTING: REMOVE
-  }
-
-  for( ILikeTurtles = 0; ILikeTurtles < UCHAR_MAX; ILikeTurtles++ )//prints counts of each uchar from inFile                 //USED FOR TESTING: REMOVE
-    cout<<ILikeTurtles<<probability[ILikeTurtles]<<endl;                                                                     //USED FOR TESTING: REMOVE
-
-  //cout<<"3"<<endl;                                                                                                         //USED FOR TESTING: REMOVE
-  outf.close();                                                                                                              //USED FOR TESTING: REMOVE
-  inFile.close();
-  return probability;
+	//cout<<"3"<<endl;                                                                                                    //USED FOR TESTING: REMOVE
+	outf.close();                                                                                                         //USED FOR TESTING: REMOVE
+	inFile.close();
+	return probability;
 
 }
 
@@ -49,7 +47,7 @@ int main()
   long long * pr = inFiProbs("lec12.pptx", "testy.pptx");
   unsigned char q =0;
   for( q = 0; q < UCHAR_MAX; q++ )//prints counts of each uchar from inFile                 //USED FOR TESTING: REMOVE
-    cout<<q<<pr[q]<<endl;
+    cout<< q << pr[q] <<endl;
 
 /*///////////////////////////////////////
   MAKE WORK WITH DIRCTORIES
