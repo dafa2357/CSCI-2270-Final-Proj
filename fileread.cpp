0fa3ab631bdaef3ab440acce646c5bdabe1cfea7
thewilltotick <49620762+thewilltotick@users.noreply.github.com>
2019-04-19 00:24:04 -0600

    Update fileread.cpp

--- fileread.cpp
+++ fileread.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 //type handling for input and output use BOOST FILESYSTEM LIBRARY
 
-long long * inFiProbs(string inFilename){// , string oFile){///REMOVE ALL instaces of output file
+unsigned long long * inFiProbs(string inFilename){// , string oFile){///REMOVE ALL instaces of output file
 	ifstream inFile;//creatae ifstream;
 	inFile.open( inFilename, ios::in|ios::binary);//open file as binary input
 	if(!(inFile.is_open()) || !(inFile.good())) {//check if open and good
@@ -14,8 +14,8 @@ long long * inFiProbs(string inFilename){// , string oFile){///REMOVE ALL instac
 		inFile.close();
 		return nullptr;
   	}
-	
-	long long * probability = new long long [UCHAR_MAX + 1];//creates probability array
+
+	unsigned long long * probability = new unsigned long long [UCHAR_MAX + 1];//creates probability array
 	for( int i = 0; i <= UCHAR_MAX ; i++){//set all counts to zero //using unsigned char ++ overloads for() ++ iteratore????
 		probability[i] = 0;
 	}
@@ -23,7 +23,7 @@ long long * inFiProbs(string inFilename){// , string oFile){///REMOVE ALL instac
   	unsigned char ILikeTurtles = 0; //temp storage of single byte
   	std::streambuf * inF = inFile.rdbuf();//creates accesible stream buffer
 	//ofstream outf ( oFile, std::ofstream::binary| std::ofstream::trunc);//creates output file of same type               //USED FOR TESTING: REMOVE
-	
+
 	while( inF->sgetc() != EOF){ //checks for valid input
     		ILikeTurtles = inF->sbumpc();//gets uchar from file moves to next position in streambuf
   		//  cout<<":"<<ILikeTurtles                                                                                  //USED FOR TESTING: REMOVE
@@ -41,7 +41,7 @@ long long * inFiProbs(string inFilename){// , string oFile){///REMOVE ALL instac
 
 int main()
 {
-	long long * pr = inFiProbs("lec12.pptx", "testy.pptx");
+	unsigned long long * pr = inFiProbs("lec12.pptx");
 	unsigned char q =0;
 	for( q = 0; q < UCHAR_MAX; q++ )//prints counts of each uchar from inFile                 //USED FOR TESTING: REMOVE
 		cout<< q << pr[q] <<endl;
